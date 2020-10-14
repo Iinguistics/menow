2b073de29ebc8367a402938995af6b195082bede
Linguistics <thylinguistics@gmail.com>
2020-10-14 16:51:35 -0700

    if the user already has a stream, router pushes back to / page

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,19 +1,43 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { createStream } from '../../actions';
+import { createStream, fetchStreams } from '../../actions';
 import StreamForm from './StreamForm';
+import history from '../../history';
 
 
 class StreamCreate extends Component {
-    
+    constructor(props){
+      super(props)
+    }
 
+    componentDidMount(){
+      this.props.fetchStreams();
+      this.viewStreams();
+    }
+    componentDidUpdate(){
+      this.viewStreams();
+    }
+
+     
+      viewStreams = ()=>{
+          if(this.props.streams !== null)
+          return this.props.streams.map((stream)=>{
+            if(stream.userId === this.props.currentUserId || this.props.isSignedIn !== true){
+              return  history.push('/');
+            }else{
+             return null
+            }
+          });
+       };
+
+
+  
           onSubmit = (formValues)=>{
             this.props.createStream(formValues);
          }
 
         
 
-   
         render(){
         return (
            <div>
@@ -26,9 +50,12 @@ class StreamCreate extends Component {
 
 
 
+  const mapStateToProps = (state, ownProps)=>{
+    return { isSignedIn: state.auth.isSignedIn,
+      streams: Object.values(state.streams),
+      currentUserId: state.auth.userId
+   }
+  }
 
 
-
-
-
-export default connect(null, {createStream}) (StreamCreate);
+export default connect(mapStateToProps, { createStream, fetchStreams }) (StreamCreate);
