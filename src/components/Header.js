53ebe4e4ca5117ad37ae01524e5ac07d44587a0c
Linguistics <thylinguistics@gmail.com>
2020-10-14 16:43:04 -0700

    if the user already has a stream, create stream option disappears from navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,23 +1,52 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import GoogleAuth from './GoogleAuth';
+import { fetchStreams } from '../actions';
 
 
-const Header = ({ isSignedIn })=>{
-   
+const Header = ({ isSignedIn, streams, currentUserId })=>{
+     const [hasStream, setHasStream] = useState(false);
+
+     useEffect(()=>{
+        fetchStreams();
+        viewStreams();
+       }, [streams]);
+
+       const renderCreateStream = ()=>{
+            if(hasStream === false && isSignedIn){
+                return(
+                 <Link to="/streams/new" className="item">
+                  Create a stream
+                 </Link>
+                )
+            }
+       }
+
+
+       const renderAdmin =(stream)=>{
+        if(stream.userId === currentUserId){
+           setHasStream(true);
+         }
+        }
+        
+    
+        const viewStreams = ()=>{
+            if(streams !== null)
+            return streams.map((stream)=>{
+                return renderAdmin(stream)
+            })
+         };
+         
+
 
       const renderKey = ()=>{
           if(isSignedIn){
-             
           return (
             <div className="right menu">
             <Link to="/mykey" className="item">
                 View your streaming key
             </Link>
-            <Link to="/streams/new" className="item">
-                Create a stream
-             </Link>
             </div>
          )
        }
@@ -35,6 +64,7 @@ const Header = ({ isSignedIn })=>{
                All Streams
             </Link>
             {renderKey()}
+            {renderCreateStream()}
             <GoogleAuth/>
          </div>
         </div>
@@ -42,8 +72,11 @@ const Header = ({ isSignedIn })=>{
 };
 
 const mapStateToProps = (state)=>{
-    return { isSignedIn: state.auth.isSignedIn }
+    return { isSignedIn: state.auth.isSignedIn,
+             streams: Object.values(state.streams),
+             currentUserId: state.auth.userId
+    }
 }
 
 
-export default connect(mapStateToProps) (Header);
+export default connect(mapStateToProps, { fetchStreams }) (Header);
